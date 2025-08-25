f3d2868b7b38335b1bd94d67ad13f2d947be1fd6
Ashishpathak78 <ap10ashu@gmail.com>
2025-08-25 15:26:04 +0530

    created catagories.jsx file

---
+++ app/(tabs)/catagories.jsx
@@ -0,0 +1,16 @@
+// import 'react-native-gesture-handler';
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+const Catagories = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Catagories Screen</Text>
+
+      <View style={{ height: 20 }} />
+      <Text>More features coming soon!</Text>
+    </View>
+  );
+}
+export default Catagories;
